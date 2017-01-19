a1d17a8ea290fcb154f3fae6da716e6a70f42478
IvanDobrovolsky <dobrovolsky94@gmail.com>
2017-01-20 01:32:33 +0200

    Added main class basic implementation

--- pure-typescript/src/app.ts
+++ pure-typescript/src/app.ts
@@ -1,1 +1,13 @@
-console.log("Up and running...");
+class WidgetApp {
+    private readonly apiUrl = 'http://api.nytimes.com/svc/mostpopular/v2/mostviewed/movies/30.json?api-key=52c786f7d5fcb689e304bcbd58687057%3A5%3A73132144';
+
+    public initialize (): void {
+        /*
+        *  getData(this.apiUrl);
+        *  render();
+        * */
+    }
+}
+
+const widgetApp = new WidgetApp();
+widgetApp.initialize();
