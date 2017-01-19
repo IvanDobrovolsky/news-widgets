2f68d68af5c1b5a795cbc0b960115ba61cc2e0c0
IvanDobrovolsky <dobrovolsky94@gmail.com>
2017-01-20 01:24:26 +0200

    Created Post model

---
+++ pure-typescript/src/models/post.model.ts
@@ -0,0 +1,10 @@
+export class Post {
+    constructor(
+        public id: number | string,
+        public title: string,
+        public img: string,
+        public description: string,
+        public link: string,
+        public date: Date) {
+    }
+}
