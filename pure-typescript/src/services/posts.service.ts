da0dfaf9631a1f2c3633c08b2e1bb67435136c99
IvanDobrovolsky <dobrovolsky94@gmail.com>
2017-01-20 02:44:33 +0200

    Implemented PostsService

---
+++ pure-typescript/src/services/posts.service.ts
@@ -0,0 +1,20 @@
+import { Post } from "./../models/post.model";
+
+/**
+ It is needed to extract posts array out of the response from the api server
+ */
+export class PostsService {
+    //TODO use Response type instead of any
+    public static extractPosts (response: any): Array<Post> {
+        return response.results.map((post: any, index: number): Post => {
+            return {
+                id: index,
+                link: post.url,
+                img: (((post.media[0])["media-metadata"])[2]).url,
+                description: <string>post.abstract,
+                title: post.title,
+                date: <Date>post.published_date
+            }
+        });
+    }
+}
